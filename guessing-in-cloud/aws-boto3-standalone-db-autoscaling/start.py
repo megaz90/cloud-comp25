6e2e382baf701ced910bc669915409c616705a59
megaz90 <furqanamjad150@gmail.com>
2024-09-12 21:53:56 +0200

    other changes

--- guessing-in-cloud/aws-boto3-standalone-db-autoscaling/start.py
+++ guessing-in-cloud/aws-boto3-standalone-db-autoscaling/start.py
@@ -10,19 +10,12 @@ from botocore.exceptions import ClientError
 #
 ################################################################################################
 
-# print("!!!!!!!! You cannot use Auto Scaling Group in AWS Educate Account !!!!!!!!")
-# exit(-1)
-
-# place your credentials in ~/.aws/credentials, as mentioned in AWS Educate Classroom,
-# Account Details, AWC CLI -> Show (Copy and paste the following into ~/.aws/credentials)
-
 # changed to use us-east, to be able to use AWS Educate Classroom
 region = 'us-east-1'
 availabilityZone1 = 'us-east-1a'
 availabilityZone2 = 'us-east-1b'
 availabilityZone3 = 'us-east-1c'
-# region = 'eu-central-1'
-# availabilityZone = 'eu-central-1b'
+
 
 # AMI ID of Amazon Linux 2 image 64-bit x86 in us-east-1 (can be retrieved, e.g., at
 # https://console.aws.amazon.com/ec2/v2/home?region=us-east-1#LaunchInstanceWizard:)
@@ -31,17 +24,10 @@ imageId = 'ami-0d5eff06f840b45e9'
 # for eu-central-1, AMI ID of Amazon Linux 2 would be:
 # imageId = 'ami-0cc293023f983ed53'
 
-# potentially change instanceType to t2.micro for "free tier" if using a regular account
-# for production, t3.nano seams better
-# as of SoSe 2022 t2.nano seams to be a bit too low on memory, mariadb first start can fail
-# due to innodb cache out of memory, therefore t2.micro or swap in t2.nano currently recommended
-# instanceType = 't2.nano'
 instanceType = 't2.micro'
 
-# keyName = 'srieger-pub'
 keyName = 'vockey'
 
-# see, e.g., AWS Academy Lab readme, or "aws iam list-instance-profiles | grep InstanceProfileName"
 # for roles see: "aws iam list-roles | grep RoleName"
 iamRole = 'LabInstanceProfile'
 
@@ -52,43 +38,60 @@ iamRole = 'LabInstanceProfile'
 #
 ################################################################################################
 
-
+# Initialize clients
 client = boto3.setup_default_session(region_name=region)
 ec2Client = boto3.client("ec2")
 ec2Resource = boto3.resource('ec2')
-
 elbv2Client = boto3.client('elbv2')
 asClient = boto3.client('autoscaling')
 
-# if you only have one VPC, vpc_id can be retrieved using:
+# Initialize a DynamoDB client
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+# VPC, Subnet, and Security Group Setup
 response = ec2Client.describe_vpcs()
 vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
-# if you have more than one VPC, vpc_id should be specified, and code
-# top retrieve VPC id below needs to be commented out
-# vpc_id = 'vpc-eedd4187'
 
-subnet_id1 = ec2Client.describe_subnets(
-    Filters=[
-        {
-            'Name': 'availability-zone', 'Values': [availabilityZone1]
-        }
-    ])['Subnets'][0]['SubnetId']
+subnet_id1 = ec2Client.describe_subnets(Filters=[{'Name': 'availability-zone', 'Values': [availabilityZone1]}])['Subnets'][0]['SubnetId']
+subnet_id2 = ec2Client.describe_subnets(Filters=[{'Name': 'availability-zone', 'Values': [availabilityZone2]}])['Subnets'][0]['SubnetId']
+subnet_id3 = ec2Client.describe_subnets(Filters=[{'Name': 'availability-zone', 'Values': [availabilityZone3]}])['Subnets'][0]['SubnetId']
 
-subnet_id2 = ec2Client.describe_subnets(
-    Filters=[
-        {
-            'Name': 'availability-zone', 'Values': [availabilityZone2]
-        }
-    ])['Subnets'][0]['SubnetId']
+# Function to create a DynamoDB table if it doesn't exist
+def create_dynamodb_table():
+    try:
+        table = dynamodb.create_table(
+            TableName='cloud_guessing_game',
+            KeySchema=[{'AttributeName': 'cloud_id', 'KeyType': 'HASH'}],
+            AttributeDefinitions=[{'AttributeName': 'cloud_id', 'AttributeType': 'N'}],
+            ProvisionedThroughput={'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5}
+        )
+        table.wait_until_exists()
+        print(f"DynamoDB Table '{table.table_name}' created successfully.")
+    except ClientError as e:
+        if 'ResourceInUseException' in str(e):
+            print("DynamoDB Table already exists. Skipping creation.")
+        else:
+            raise e
 
-subnet_id3 = ec2Client.describe_subnets(
-    Filters=[
-        {
-            'Name': 'availability-zone', 'Values': [availabilityZone3]
-        }
-    ])['Subnets'][0]['SubnetId']
+# Function to insert data into DynamoDB table
+def insert_instance_data(instance_id, instance_type, private_ip, status):
+    table = dynamodb.Table('cloud_guessing_game')
+    try:
+        table.put_item(
+            Item={
+                'cloud_id': int(instance_id[-5:], 16),  # Generate cloud_id based on instance_id (for uniqueness)
+                'instance_id': instance_id,
+                'instance_type': instance_type,
+                'private_ip': private_ip,
+                'status': status
+            }
+        )
+        print(f"Inserted instance data for {instance_id} into DynamoDB.")
+    except ClientError as e:
+        print(f"Error inserting instance data: {e}")
+        
 
-
+#Deleting old configurations
 print("Deleting old auto scaling group...")
 print("------------------------------------")
 
@@ -164,6 +167,11 @@ try:
 except ClientError as e:
     print(e)
 
+print("Create Dynamo DB tables")
+print("------------------------------------")
+# Creating DynamoDB table (if it doesn't exist)
+create_dynamodb_table()
+
 print("Create security group...")
 print("------------------------------------")
 
@@ -202,24 +210,8 @@ print("Running new DB instance...")
 print("------------------------------------")
 
 userDataDB = ('#!/bin/bash\n'
-              '# extra repo for RedHat rpms\n'
-              'yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm\n'
-              '# essential tools\n'
               'yum install -y joe htop git\n'
-              '# mysql\n'
-              'yum install -y mariadb mariadb-server\n'
-              '\n'
-              'service mariadb start\n'
-              '\n'
-              'echo "create database cloud_guessing_game" | mysql -u root\n'
-              '\n'
-              'echo "create table clouds ( cloud_id INT AUTO_INCREMENT, name VARCHAR(255) NOT NULL, value INT, max_value INT, PRIMARY KEY (cloud_id))" | mysql -u root cloud_guessing_game\n'
-              '\n'
-              'echo "CREATE USER \'cloud_guessing_game\'@\'%\' IDENTIFIED BY \'cloudpass\';" | mysql -u root\n'
-              'echo "GRANT ALL PRIVILEGES ON cloud_guessing_game.* TO \'cloud_guessing_game\'@\'%\';" | mysql -u root\n'
-              'echo "FLUSH PRIVILEGES" | mysql -u root\n'
               )
-# convert user-data from script with: cat install-mysql | sed "s/^/'/; s/$/\\\n'/"
 
 response = ec2Client.run_instances(
     ImageId=imageId,
@@ -229,9 +221,7 @@ response = ec2Client.run_instances(
     MinCount=1,
     MaxCount=1,
     UserData=userDataDB,
-    SecurityGroupIds=[
-        security_group_id,
-    ],
+    SecurityGroupIds=[security_group_id,],
     TagSpecifications=[
         {
             'ResourceType': 'instance',
@@ -245,20 +235,16 @@ response = ec2Client.run_instances(
 
 instanceIdDB = response['Instances'][0]['InstanceId']
 privateIpDB = response['Instances'][0]['PrivateIpAddress']
-# privateIpDB = response['Instances'][0]['NetworkInterfaces'][0]['NetworkInterfaceId']
-
 instance = ec2Resource.Instance(instanceIdDB)
 instance.wait_until_running()
 
 print(instanceIdDB)
 
 userDataWebServer = ('#!/bin/bash\n'
-                     '# extra repo for RedHat rpms\n'
-                     'yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm\n'
                      '# essential tools\n'
                      'yum install -y joe htop git\n'
-                     '# mysql\n'
-                     'yum install -y httpd php php-mysql\n'
+                     '# Install AWS CLI or SDK if needed\n'
+                     'yum install -y aws-cli\n'
                      '\n'
                      'service httpd start\n'
                      '\n'
@@ -385,15 +371,19 @@ response = asClient.put_scaling_policy(
     }
 )
 
+# Example of querying DynamoDB to check instance data
+def query_instance_data(instance_id):
+    table = dynamodb.Table('cloud_guessing_game')
+    try:
+        response = table.get_item(Key={'cloud_id': int(instance_id[-5:], 16)})
+        if 'Item' in response:
+            print(f"Queried instance data: {response['Item']}")
+        else:
+            print(f"No data found for instance {instance_id}")
+    except ClientError as e:
+        print(f"Error querying instance data: {e}")
+
+# Example of querying the instance data
+query_instance_data(instanceIdDB)
+
 print('Load Balancer should be reachable at: http://' + loadbalancer_dns)
-
-print('As always, you need to wait some time, until load balancer is provisioned, instances are healthy (cloud-init '
-      'did its job as specified in the launch configuration). ')
-
-print('You can use "aws elbv2 ..." commands or the web console to examine the current state. Take a look at Load'
-      'Balancer, Target Group, Auto Scaling Group and esp. Monitoring of the Load Balancer and related Cloud Watch'
-      'alarms.')
-
-print('If you "pull" a lot of clouds in the game, generating a lot of requests, you will see the alarm being fired and'
-      'further instances started (scale-out) (involves some clicking for about three minutes). After 15 min of idling,'
-      'instances will automatically be stopped (scale-in).')
